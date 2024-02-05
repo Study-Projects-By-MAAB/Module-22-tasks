95b479e3af2b83b73ab811f602535d3790e43aef
maabfw <maabfw@proton.me>
2024-02-05 11:56:58 +0600

    4task done

--- task/4task.js
+++ task/4task.js
@@ -1,6 +1,4 @@
-You are given an array of phone objects, each containing information about the model, brand, and price.Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and returns the average price of phone.
-
-    Input
+// You are given an array of phone objects, each containing information about the model, brand, and price.Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and returns the average price of phone.
 
 
 const phones = [
@@ -11,3 +9,15 @@ const phones = [
     { model: "PhoneE", brand: "Iphone", price: 105000 },
     { model: "PhoneF", brand: "HTC", price: 48000 },
 ];
+
+
+function findAveragePhonePrice(phones) {
+    let sum = 0;
+    for (const phone of phones) {
+        sum += phone.price;
+    }
+    return parseInt(sum / phones.length);
+
+}
+const avg = findAveragePhonePrice(phones)
+console.log('Average Phone price is', avg);
